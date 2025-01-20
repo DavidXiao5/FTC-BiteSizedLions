4abce5e1d9a4ccd05ca157ebeec9705bcfc14934
David Xiao <132177864+DavidXiao5@users.noreply.github.com>
2025-01-20 10:09:20 -0600

    1/19 Voyagers Code

---
+++ TeamCode/src/main/java/TeleOpv2/DriveTrainv2.java
@@ -0,0 +1,52 @@
+package TeleOpv2;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class DriveTrainv2 {
+
+    public void moveRobot(double y, double x, double rx, HardwareMap hardwareMap){
+
+        // Declare motors, make them run w/o encoder
+        DcMotor frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        DcMotor backLeft = hardwareMap.dcMotor.get("backLeft");
+        DcMotor frontRight = hardwareMap.dcMotor.get("frontRight");
+        DcMotor backRight = hardwareMap.dcMotor.get("backRight");
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Make the motors orientation right
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Set ZeroPower to be brake to actively stop the robot
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Set initial power to zero
+        frontLeft.setPower(0);
+        backLeft.setPower(0);
+        frontRight.setPower(0);
+        backRight.setPower(0);
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio,adb
+        // but only if at least one is out of the range [-1, 1]
+
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
+
+        frontLeft.setPower(frontLeftPower);
+        backLeft.setPower(backLeftPower);
+        frontRight.setPower(frontRightPower);
+        backRight.setPower(backRightPower);
+    }
+}
--- TeamCode/src/main/java/TeleOpv2/newTeleTest.java
+++ TeamCode/src/main/java/TeleOpv2/newTeleTest.java
@@ -1,34 +1,106 @@
+//for motors the POWER ranges from a value from -1 to 1, this is continuous (although it can be set to a position like a servo)
+
+//meanwhile servos can be powered by setting the position OR continuous
+
 package TeleOpv2;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
-
-@TeleOp
+@TeleOp()
 public class newTeleTest extends LinearOpMode {
-    @Override
-    public void runOpMode() throws InterruptedException {
 
-        // Wait for start
+    @Override //Overrides methods of the superclass
+
+    public void runOpMode() {
+
+        DriveTrainv2 drivetrain = new DriveTrainv2();
+
+        DcMotor slides = hardwareMap.get(DcMotor.class, "slidesMotor");
+        DcMotor pivot = hardwareMap.get(DcMotor.class, "pivot");
+
+        Servo cpivot = hardwareMap.get(Servo.class,"cpivot");
+        Servo claw = hardwareMap.get(Servo.class, "claw");
+
+        cpivot.scaleRange(0, 0.25); //this will set the limit for how much the servo can rotate
+        claw.scaleRange(0, 0.5); // make sure to change it later
+
+        slides.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        pivot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         waitForStart();
-
-        if (isStopRequested()) return;
-
-        DcMotor pivot = hardwareMap.dcMotor.get("pivot");
-        pivot.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        telemetry.update();
-
-        // Actual OpMode
         while (opModeIsActive()) {
-            if (gamepad1.a){
-                pivot.setPower(0.1);
+
+            // Drive Train Buttons
+            if (gamepad1.left_bumper) {
+                double y = -gamepad1.left_stick_y * .5;
+                double x = gamepad1.left_stick_x * .55;
+                double rx = gamepad1.right_stick_x * .5;
+                drivetrain.moveRobot(y, x, rx, hardwareMap);
             }
-            else if (gamepad1.b){
-                pivot.setPower(-0.1);
+
+            else if (gamepad1.right_bumper) {
+                double y = -gamepad1.left_stick_y * .25;
+                double x = gamepad1.left_stick_x * .275;
+                double rx = gamepad1.right_stick_x * .25;
+                drivetrain.moveRobot(y, x, rx, hardwareMap);
+            }
+
+            else {
+                double y = -gamepad1.left_stick_y;
+                double x = gamepad1.left_stick_x * 1.1;
+                double rx = gamepad1.right_stick_x;
+                drivetrain.moveRobot(y, x, rx, hardwareMap);
+            }
+
+
+            // Slides
+            boolean rb = gamepad1.right_bumper;
+            boolean lb = gamepad1.left_bumper;
+
+            if(rb && slides.getCurrentPosition() < 3000){ // If the Right Button is pressed and it's under the maximum
+                slides.setPower(0.2);
+
+            }
+            else if(lb && slides.getCurrentPosition() > -20){ // If the Left Button is pressed and it's over the minimum
+                slides.setPower(-0.2);
+
             }
             else{
-                pivot.setPower(0);
+                slides.setPower(0);
+            }
+
+
+            // Pivot
+            double rt = gamepad1.right_trigger;
+            double lt = gamepad1.left_trigger;
+
+            if(pivot.getCurrentPosition() < 1500 && pivot.getCurrentPosition() > 0) {
+                if (rt - lt < 0.5 && rt - lt > -0.5) {
+                    pivot.setPower(rt - lt); //this sets the power to be the difference of the right button (pivoting forward - range of 0 to 1) and the back button (pivoting back - range of 0 to 1)
+                }
+            }
+
+            // Claw Pivot
+            boolean a = gamepad1.a;
+            boolean b = gamepad1.b;
+            if(a){
+                cpivot.setPosition(1);
+            }
+            else if(b){
+                cpivot.setPosition(0);
+            }
+
+            // Claw
+            boolean x_button = gamepad1.x;
+            boolean y_button = gamepad1.y;
+            if(x_button){
+                claw.setPosition(1);
+            }
+            else if(y_button){
+                claw.setPosition(0);
             }
         }
     }
